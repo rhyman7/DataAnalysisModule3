13d1c468651da9e01af6fbacdf509c70741671bd
Ryan Hyman <ryan.hyman7@gmail.com>
2026-02-02 19:50:58 -0500

    SQL_basics exercise

--- Week1/AssignmentFiles/SQL_basics.sql
+++ Week1/AssignmentFiles/SQL_basics.sql
@@ -7,24 +7,39 @@ USE coffeeshop_db;
 -- =========================================================
 
 -- Q1) List all products (show product name and price), sorted by price descending.
-
+select name, price from products
+order by price desc;
 -- Q2) Show all customers who live in the city of 'Lihue'.
-
+select * from customers where city = "Lihue";
 -- Q3) Return the first 5 orders by earliest order_datetime (order_id, order_datetime).
+select order_datetime from orders
+order by order_datetime asc
+limit 5;
 
 -- Q4) Find all products with the word 'Latte' in the name.
-
+select * from products;
+select * from products where name like '%Latte%'; 
 -- Q5) Show distinct payment methods used in the dataset.
+select distinct payment_method
+from orders;
 
 -- Q6) For each store, list its name and city/state (one row per store).
+select * from stores;
 
 -- Q7) From orders, show order_id, status, and a computed column total_items
 --     that counts how many items are in each order.
+select * from orders;
 
 -- Q8) Show orders placed on '2025-09-04' (any time that day).
+select order_datetime from orders
+where DATE(order_datetime) like '%2025-09-04%';
 
 -- Q9) Return the top 3 most expensive products (price, name).
+select name, price from products
+order by price desc
+limit 3;
 
 -- Q10) Show customer full names as a single column 'customer_name'
 --      in the format "Last, First".
+select last_name, fist_name from customers;
 
